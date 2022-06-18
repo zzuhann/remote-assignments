3d603c80c995aee56d7bd944478894c188145b7d
zzuhann <zzuhanlin@gmail.com>
2022-06-18 23:19:06 +0800

    show and hide menu

--- week-2/index.js
+++ week-2/index.js
@@ -1,7 +1,6 @@
 const bannerText = document.getElementById('banner-text');
 bannerText.addEventListener('click', changeText);
 
-
 function changeText() {
     bannerText.textContent = 'Have a Good Time!'
 }
@@ -13,6 +12,7 @@ let w = window.innerWidth;
 
 function toggleBoxes() {
     if (bottomBox.style.display === 'none') {
+        w = window.innerWidth;
         switch (true) {
             case w >= 500:
                 return bottomBox.style.display = 'flex';
@@ -24,4 +24,23 @@ function toggleBoxes() {
     } else {
         bottomBox.style.display = 'none';
     }
+}
+
+// 選單
+
+const showMenuBtn = document.getElementById('menu-img');
+const menu = document.querySelector('.menu');
+const body = document.body;
+const hideMenuBtn = document.getElementById('close-menu')
+showMenuBtn.addEventListener('click', showMenu);
+hideMenuBtn.addEventListener('click', hideMenu);
+
+function showMenu() {
+    menu.style.right = '0';
+    body.style.overflow = 'hidden';
+}
+
+function hideMenu() {
+    menu.style.right = '-50%';
+    body.style.overflow = 'scroll';
 }
