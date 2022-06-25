268caaa3a54f13a9e18c052256276133c1cbd1b3
zzuhann <zzuhanlin@gmail.com>
2022-06-25 23:23:18 +0800

    assignment 1 and 2

--- week-3/app.js
+++ week-3/app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index');
+})
+// app.get('/getData', (req, res) => {
+//     res.render('getData');
+// })
+
+app.get('/getData', (req, res, next) => {
+    const number = req.query.number;
+    let result;
+    if (req.url === '/getData') {
+        next();
+    }
+    else if (req.url !== '/getData' && number > 0) {
+        result = number * (number + 1) / 2;
+    } else {
+        result = 'Wrong Parameter';
+    }
+    res.render('getData', { result, number })
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
+});
